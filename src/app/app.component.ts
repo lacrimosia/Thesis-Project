84b8163f184d3c00f2266f8341fa7c6139b05c43
Tila <cjsgamer@gmail.com>
2017-11-19 00:39:08 -0800

    Added new data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,21 +12,20 @@ import { DataService } from './data.service';
 
 
 export class AppComponent implements OnInit {
-  title = 'app';
-  name = "";
+  title = "";
 
   // Inject HttpClient into your component or service.
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.getAllData("../assets/data/test.json");
+    this.getAllData("../assets/data/bp.json");
   }
 
   getAllData(url){
   	// Make the HTTP request:
     this.dataService.getData(url).subscribe(data => {
       // Read the result field from the JSON response.
-      this.name = data.name;
+      this.title = data.intro.title;
     });
   }
 
